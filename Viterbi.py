7c104dad22af57996fb09fd3dd994af8398a61e3
alexzamurca <alexzamurca@yahoo.co.uk>
2021-04-30 09:46:17 +0100

    algorithm didn't work previously, works now

--- Viterbi.py
+++ Viterbi.py
@@ -5,6 +5,8 @@ import math
 
 class Viterbi:
     calculated_values = None
+    calculated_max_values = None
+    decisions = None
     sequence = None
 
     def __init__(self, S: list, T: np.array, pi: np.array, Sigma: list, E: np.array):
@@ -20,17 +22,18 @@ class Viterbi:
             return
 
         # initialise the values we calculate
-        self.calculated_values = np.zeros((len(self.S), len(observation_sequence)))
+        self.calculated_values = np.zeros((len(self.S), len(observation_sequence)), dtype=np.double)
+        self.calculated_max_values = np.zeros((len(self.S), len(observation_sequence)), dtype=np.double)
+        self.decisions = np.zeros((len(self.S), len(observation_sequence)), dtype=int)
         for i in range(len(start_values)):
             self.calculated_values[i][0] = start_values[i]
         self.sequence = np.chararray((1, len(observation_sequence)))
-        self.sequence[0][0] = self.S[self.__get_state_from_max_list(start_values)[1]]
 
         # go through sequence ignoring the start char
-        for i in range(1, len(observation_sequence)):
+        for i in range(1, len(observation_sequence) + 1):
             for s in range(len(self.S)):
-                max_list = np.zeros(len(self.S), dtype=float)
-                #max_list_str = ""
+                max_list = np.zeros(len(self.S), dtype=np.double)
+                # max_list_str = ""
                 # form the list that we use in the max
                 # for each state
                 for state in range(len(self.S)):
@@ -42,26 +45,32 @@ class Viterbi:
                     """
 
                 # once we have the max list choose a max and note down what state we chose
-                max_state = np.max(max_list)
+                max_state, state_index = self.__get_state_from_max_list(max_list)
 
-                self.calculated_values[s][i] = self.E[s][observation_sequence[i]] + max_state
-                #print()
-                #print("P_" + self.S[s] + "(" + self.Sigma[observation_sequence[i]] + ", " + str(i) + ") = e_" + self.S[s] + "(" + self.Sigma[observation_sequence[i]] + "):{" + str(self.E[s][observation_sequence[i]]) + "} + max(" + max_list_str + "):{" + str(max_state) + "}")
-                #print()
+                if not i >= len(observation_sequence):
+                    self.calculated_values[s][i] = np.double(self.E[s][observation_sequence[i]]) + max_state
+                    self.decisions[s][i] = state_index
 
-        self.__compute_sequence()
+                self.calculated_max_values[s][i-1] = max_state
 
-    def __compute_sequence(self):
-        for column in range(len(self.calculated_values[0])):
+                # print()
+                # print("P_" + self.S[s] + "(" + self.Sigma[observation_sequence[i]] + ", " + str(i) + ") = e_" + self.S[s] + "(" + self.Sigma[observation_sequence[i]] + "):{" + str(self.E[s][observation_sequence[i]]) + "} + max(" + max_list_str + "):{" + str(max_state) + "}")
+                # print()
+
+        self.compute_sequence()
+
+    def compute_sequence(self) -> None:
+        # check all decisions
+        for sequence_index in range(len(self.calculated_max_values[0]) - 1, -1, -1):
+            # check for the smallest value
             current_max_value = -math.inf
             max_value_index = 0
-            for row in range(len(self.S)):
-                value = self.calculated_values[row][column]
-                if value > current_max_value:
-                    max_value_index = row
+            for state_index in range(len(self.S)):
+                value = self.calculated_max_values[state_index][sequence_index]
+                if value >= current_max_value:
+                    max_value_index = state_index
                     current_max_value = value
-
-            self.sequence[0][column] = self.S[max_value_index]
+            self.sequence[0][sequence_index] = self.S[max_value_index]
 
 
     def __get_state_from_max_list(self, max_list: np.array) -> Tuple[float, int]:
